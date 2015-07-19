90d41aee69a0125231274dcbc7f35f09f9758920
Free BSD <matt.ferrera@gmail.com>
2015-07-19 02:38:01 +0000

    1.0.0

---
+++ public/js/main.js
@@ -0,0 +1,42 @@
+$(function() {
+    $('#slider').slider({
+	value:1000,
+	min: 50,
+	max: 5000,
+	slide: function(event, ui) {
+	    searchArea.setRadius(ui.value);
+	}
+    });
+});
+
+$(document).ready(function() {
+    $('form#options').submit(function(event) {
+	event.preventDefault();
+
+	for (var i = 0; i < markers.length; i++) {
+	    markers[i].setMap(null);
+	}
+	
+	markers = [];
+	
+	var data = {};
+
+	data['lat'] = $('#formLat').val();
+	data['lng'] = $('#formLng').val();
+	data['radius'] = $('#formRadius').val();
+
+	$.ajax({
+	    'type': 'POST',
+	    'data': JSON.stringify(data),
+	    'url': '/',
+	    'dataType': 'JSON',
+	    'contentType': 'application/json',
+	    'success': function(data, textStatus, jqXHR) {
+		handleData(data);
+	    },
+	    'error': function(jqXHR, textStatus, errorThrown) {
+		alert(errorThrown);
+	    }
+	});
+    });
+});
---
+++ routes/index.js
@@ -0,0 +1,74 @@
+var express = require('express');
+var router = express.Router();
+var geoip = require('geoip-lite');
+var https = require('https');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    // switch when behind proxy server
+    //var ip = req.headers['x-forwarded-for'];
+    var ip = req.connection.remoteAddress;
+    var geo;
+
+    if (ip.substring(0, 7) == '::ffff:') {
+	geo = geoip.lookup(ip.substring(7));
+    } else {
+	geo = geoip.lookup(ip);
+    }
+
+    iplat = geo['ll'][0];
+    iplng = geo['ll'][1];
+
+    res.render('index', {
+	'iplat': iplat,
+	'iplng': iplng
+    });
+});
+
+/* POST */
+router.post('/', function(req, res, next) {
+    var data = req.body;
+    var key = "YOURINSTAGRAMAPIKEYGOESHERE";
+
+    var options = {
+	'host': "api.instagram.com",
+	'port': 443,
+	'method': "GET",
+	'path': '/v1/media/search?lat=' + data["lat"]
+	    + '&lng=' + data["lng"]
+	    + '&distance=' + data["radius"]
+	    + '&client_id=' + key
+    }
+
+    apiCall = function(r) {
+	// container for instagram data
+	var body = '';
+
+	// put returned data into holder object
+	r.on('data', function(d) {
+	    body += d;
+	});
+
+	r.on('end', function() {
+	    body = JSON.parse(body);
+
+	    // if instagram call returns an error
+	    if (body['meta'].code == '400') {
+		res.json({'error': body['meta'].error_message});
+		return;
+	    }
+
+	    // if there are no posts in the query
+	    if (body['meta'].code == '200' && body['data'] == '') {
+		res.json({'error': 'Sorry, no results.'});
+		return;
+	    }
+
+	    res.json(body);
+	});
+    }
+
+    https.request(options, apiCall).end();
+});
+
+module.exports = router;
